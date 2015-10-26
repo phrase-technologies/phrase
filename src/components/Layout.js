c97f703e599e17bae4676f660cfae81c81355743
Anson Kao <anson.kao@gmail.com>
2015-10-26 11:45:05 -0400

    Added images to CSS and JSX. (logo)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,12 @@ import Transport from './Transport.js';
 
 export default class Layout extends Component {
   render() {
+    var logo = require('../img/phrase-logo-black-engraved-2015-10-26.png');  
+
     return (
       <div className="layout">
         <div className="layout-header">
-          <p>
-            Some random test text!
-          </p>
+          <img src={logo} />
         </div>
         <div className="layout-body">
           <p>
