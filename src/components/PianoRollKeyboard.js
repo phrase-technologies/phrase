29959c5793c8d5c69c3bff1c8435923163cb0fd0
Anson Kao <anson.kao@gmail.com>
2015-11-19 17:11:34 -0500

    Keyboard key labels added (e.g. C1, D1, E1, etc...)

--- src/components/PianoRollKeyboard.js
+++ src/components/PianoRollKeyboard.js
@@ -37,18 +37,21 @@ export default class PianoRollKeyboard extends Component {
   }
 
   renderKeys() {
+    var octave = 8;
     var octaves = [];
     var keys = [];
     for( var key = 88; key > 0; key-- )
     {
       // Fill the row for the black keys
-      var keyClass = "piano-roll-key";
+      let keyClass = "piano-roll-key";
           keyClass += ( key % 12 in {2:true, 0:true, 10: true, 7: true, 5: true} ) ? ' black' : ' white';
           keyClass += ( key % 12 in {2:true,  7:true} ) ? ' higher' : '';
           keyClass += ( key % 12 in {10:true, 5:true} ) ? ' lower' : '';
           keyClass += ( key % 12 in {6:true} ) ? ' thinner' : '';
+      let keyLabel =  {1:'A', 11:'G', 9:'F', 8:'E', 6:'D', 4:'C', 3:'B'}[ key%12 ];
+          keyLabel = keyLabel ? (keyLabel + octave) : null;
 
-      keys.push(<div key={key} className={keyClass} />);
+      keys.push(<div key={key} className={keyClass}><div className="piano-roll-key-label">{keyLabel}</div></div>);
 
       // Next keys into octave Group
       if( key % 12 === 4 || key == 1 )
@@ -56,11 +59,12 @@ export default class PianoRollKeyboard extends Component {
         // Add Octave Label for full octaves
         if( key < 83 && key > 1 )
         {
-          let label = 'C' + Math.ceil(key/12.0);
+          let label = 'C' + octave;
           keys.unshift(<div className="piano-roll-octave-label"><div>{label}</div></div>);
         }
         octaves.push(<div className="piano-roll-octave">{keys}</div>);
         keys = [];
+        octave--;
       }
     }
     return octaves;
