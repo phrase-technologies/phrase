c9eec8196f499f3dcd13e0725612becd36648754
Anson Kao <anson.kao@gmail.com>
2015-12-30 13:26:49 -0500

    Notes are translated instead of repositioned (minor perf improvement) + note hover.

--- src/components/PianoRollNote.js
+++ src/components/PianoRollNote.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 export default class PianoRollNote extends Component {
   render() {
     var noteStyle = {
-      top: this.props.top,
-      left: this.props.left,
-      width: this.props.width,
-      height: this.props.height,
+      transform: "translate3d("+this.props.left + 0.0 + 'px,' + this.props.top + 1.0 + 'px,0)',
+      // top: this.props.top + 1.0,
+      // left: this.props.left + 0.0,
+      width: this.props.width - 0.5,
+      height: this.props.height - 1.0,
     };
 
     return (
