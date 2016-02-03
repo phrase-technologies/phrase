5a72a00b021f3b7dd44364f53860417dd349f569
Anson Kao <anson.kao@gmail.com>
2016-02-03 18:46:52 -0500

    RX,js event streams / observables used to clean up Pianoroll control flow

--- src/components/PianorollWindowControl.js
+++ src/components/PianorollWindowControl.js
@@ -4,12 +4,14 @@ import provideGridSystem from './GridSystemProvider'
 import provideGridScroll from './GridScrollProvider'
 
 import _ from 'lodash'
+import Rx from 'rx'
 import { pianorollScrollX,
          pianorollScrollY,
          pianorollMoveCursor,
          pianorollSelectionStart,
          pianorollSelectionEnd } from '../actions/actionsPianoroll.js';
-import { phraseCreateNote } from '../actions/actionsPhrase.js';
+import { phraseCreateNote,
+         phraseSelectNote } from '../actions/actionsPhrase.js';
 
 export class PianorollWindow extends Component {
 
@@ -18,28 +20,6 @@ export class PianorollWindow extends Component {
 
     this.isDragging = false;
     this.lastClickTimestamp = 0
-
-    this.handleGrip = this.handleGrip.bind(this);
-    this.handleDrag = this.handleDrag.bind(this);
-    this.handleDrop = this.handleDrop.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.grid.marginTop    =  0
-    this.props.grid.marginBottom = 30
-    this.props.grid.marginLeft   = 10
-    this.props.grid.marginRight  = 10
-
-    this.container = ReactDOM.findDOMNode(this);
-    document.addEventListener("mousemove", this.handleDrag);
-    document.addEventListener("mouseup",   this.handleDrop);
-    this.container.addEventListener("mousedown", this.handleGrip);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("mousemove", this.handleDrag);
-    document.removeEventListener("mouseup",   this.handleDrop);
-    this.container.removeEventListener("mousedown", this.handleGrip);
   }
 
   render() {
@@ -50,49 +30,98 @@ export class PianorollWindow extends Component {
     );
   }
 
-  handleGrip(e) {
-    if (e.target !== this.container)
-      return
+  componentDidMount() {
+    this.props.grid.marginTop    =  0
+    this.props.grid.marginBottom = 30
+    this.props.grid.marginLeft   = 10
+    this.props.grid.marginRight  = 10
 
-    var x = this.getPercentX(e);
-    var y = this.getPercentY(e);
+    this.container = ReactDOM.findDOMNode(this);
+    this.mousedown$ = Rx.Observable.fromEvent(this.container, "mousedown")
+    this.mousemove$ = Rx.Observable.fromEvent(document, "mousemove")
+    this.mouseup$   = Rx.Observable.fromEvent(document, "mouseup")
 
-    // Doubleclick - Create Note
-    if (Date.now() - this.lastClickTimestamp < 640 && this.lastClickX == x && this.lastClickY == y) {
+    var foundNote$ = this.mousedown$.map(e => {
+      let x = this.getPercentX(e);
+      let y = this.getPercentY(e);
       let bar = (this.props.xMin + this.props.grid.getMouseXPercent(e)*this.props.grid.getBarRange()) * this.props.barCount;
       let key = Math.ceil(this.props.keyCount - (this.props.yMin + this.props.grid.getMouseYPercent(e)*this.props.grid.getKeyRange())*this.props.keyCount);
-      this.props.dispatch( phraseCreateNote(this.props.currentTrack.id, key, bar) );
+      let foundNote = this.props.notes.find(note => {
+        return (
+          Math.round(key) == note.keyNum &&
+          bar >= note.start &&
+          bar <= note.end          
+        )
+      })
+      return {
+        note: foundNote,
+        bar: bar,
+        key: key,
+        x: x,
+        y: y,
+        shiftKey: e.shiftKey
+      }
+    })
+    var emptyAreaSelected$ = foundNote$.filter(e =>  !e.note)
+    var noteSelected$      = foundNote$.filter(e => !!e.note)
 
-    // Singleclick - Selection Box
-    } else {
-      this.props.dispatch( pianorollSelectionStart(x,y) );
-
-      this.isDragging = true;
-      this.lastClickTimestamp = Date.now()
-      this.lastClickX = x
-      this.lastClickY = y
-    }
+    this.setupEmptyAreaActions(emptyAreaSelected$)
+    this.setupNoteActions(noteSelected$)
   }
 
-  handleDrag(e) {
-    // Ignore unrelated move events
-    if( !this.isDragging )
-      return;
+  // All actions that stem from an initial click in an empty part of the track
+  setupEmptyAreaActions(emptyAreaSelected$) {
+    // Event Stream Flows
+    var resizeSelectionBox$ = emptyAreaSelected$
+      .flatMapLatest(() => this.mousemove$.takeUntil(this.mouseup$))
+      .map(e => {
+        var x = this.getPercentX(e);
+        var y = this.getPercentY(e);
+        return { x, y, shiftKey: e.shiftKey }
+      })
+    var applySelectionBox$ = resizeSelectionBox$
+      .flatMapLatest(() => this.mouseup$.take(1))
+      .map(e => {
+        var x = this.getPercentX(e);
+        var y = this.getPercentY(e);
+        return { x, y, shiftKey: e.shiftKey }
+      })
+    var createNote$ = emptyAreaSelected$
+      .timeInterval()
+      .bufferWithCount(2, 1)
+      .filter(buffer => {
+        return buffer[1].interval < 640
+            && buffer[0].value.bar === buffer[1].value.bar
+            && buffer[0].value.key === buffer[1].value.key
+      })
+      .map(buffer => buffer[1].value)
 
-    let x = this.getPercentX(e);
-    let y = this.getPercentY(e);
-    this.props.dispatch( pianorollSelectionEnd(x,y) );
+    // Actions
+    let dispatch = this.props.dispatch
+    emptyAreaSelected$.subscribe(e => dispatch( pianorollSelectionStart(e.x, e.y) ) )
+    resizeSelectionBox$.subscribe(e => dispatch( pianorollSelectionEnd(e.x, e.y) ) )
+    applySelectionBox$.subscribe(e => {
+      this.props.dispatch( pianorollSelectionStart(null, null) );
+      this.props.dispatch( pianorollSelectionEnd(null, null) );
+    })
+    createNote$.subscribe(e => dispatch( phraseCreateNote(this.props.currentTrack.id, e.key, e.bar) ) )
   }
 
-  handleDrop(e) {
-    // Remove selection box
-    if (this.isDragging) {
-      // TODO: Batch these actions
-      this.props.dispatch( pianorollSelectionStart(null,null) );
-      this.props.dispatch( pianorollSelectionEnd(null,null) );      
-    }
+  // All actions that stem from an initial click on an existing note
+  setupNoteActions(noteSelected$) {
+    // Event Stream Flows
+    var deleteNote$ = noteSelected$
+      .timeInterval()
+      .bufferWithCount(2, 1)
+      .filter(buffer => buffer[1].interval < 640 && buffer[0].value.note.id === buffer[1].value.note.id)
+      .map(buffer => buffer[1].value)
 
-    this.isDragging = false;
+    // Actions
+    let dispatch = this.props.dispatch
+    noteSelected$.subscribe(e => dispatch( phraseSelectNote(this.props.currentTrack.id, e.note.id, e.shiftKey) ) )
+    deleteNote$.subscribe(deletedNote => {
+      console.log("deleteNote$:", deletedNote)
+    })
   }
 
   getPercentX(e) {
@@ -113,6 +142,7 @@ PianorollWindow.propTypes = {
   xMax:         React.PropTypes.number.isRequired,
   yMin:         React.PropTypes.number.isRequired,
   yMax:         React.PropTypes.number.isRequired,
+  currentTrack: React.PropTypes.object,
   notes:        React.PropTypes.array.isRequired
 }
 
