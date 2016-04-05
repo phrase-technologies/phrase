4cd4fdc3e8003cae6e45b01e5477d5a36875e145
Alex Wilmer <djazium@gmail.com>
2016-04-04 23:00:31 -0400

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import convert from 'koa-convert'
 import bodyParser from 'koa-bodyparser'
+import cors from 'koa-cors';
 import mongoose from 'mongoose'
 import { db } from './config'
 import auth from './auth'
@@ -14,12 +15,13 @@ let app = new Koa()
 
 app.keys = [ `i got 99 problemz but a DAW aint one` ]
 
+app.use(convert(cors()))
 app.use(convert(bodyParser()))
 app.use(auth.routes())
 app.use(convert(jwt({ secret })))
 
-app.listen(3000, () => {
-  console.log(chalk.yellow(`⚡ Server running at http://localhost:3000`))
+app.listen(5000, () => {
+  console.log(chalk.yellow(`⚡ Server running at http://localhost:5000`))
 })
 
 export default app
