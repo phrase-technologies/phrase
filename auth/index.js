ca83cd2e1999708c182928fe3669580e6634ce50
Anson Kao <anson.kao@gmail.com>
2016-04-13 01:34:33 -0400

    Log in endpoint - validation messages adjusted + simple code cleanup/linting

--- auth/index.js
+++ auth/index.js
@@ -14,7 +14,7 @@ export default ({
         if (err) throw err
         if (user) res.json({
           success: false,
-          message: `User with this email already exists.`,
+          message: `An account with this email already exists.`,
         })
         else {
 
@@ -45,25 +45,26 @@ export default ({
       if (err) throw err
 
       if (!user) {
-        res.json({ success: false, message: 'User not found.' })
-      } else if (user) {
+        res.json({
+          success: false,
+          message: `User not found.`,
+        })
+      } else if (user.password !== password) {
+        res.json({
+          success: false,
+          message: `Bad email/password combination.`,
+        })
+      } else {
+        let token = jwt.sign(user, app.get(`superSecret`), {
+          expiresInMinutes: 1440, // expires in 24 hours
+        })
 
-        // check if password matches
-        if (user.password !== password) {
-          res.json({ success: false, message: 'Wrong password.' })
-        } else {
-
-          let token = jwt.sign(user, app.get('superSecret'), {
-            expiresInMinutes: 1440, // expires in 24 hours
-          })
-
-          res.json({
-            success: true,
-            message: 'Enjoy your token!',
-            token,
-            user,
-          })
-        }
+        res.json({
+          success: true,
+          message: `Enjoy your token!`,
+          token,
+          user,
+        })
       }
     })
   })
@@ -75,18 +76,18 @@ export default ({
     if (token) {
       jwt.verify(token, app.get(`superSecret`), (err, decoded) => {
         if (err) {
-          return res.json({ success: false, message: 'Failed to authenticate token.' })
-        } else {
-          req.decoded = decoded
-          next()
+          return res.json({ success: false, message: `Failed to authenticate token.` })
         }
+
+        req.decoded = decoded
+        next()
       })
 
     } else {
 
       return res.status(403).send({
         success: false,
-        message: 'No token provided.',
+        message: `No token provided.`,
       })
     }
   })
