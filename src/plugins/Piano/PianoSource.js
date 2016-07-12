6b4fbd34f42cb5511cb5eb0d67852a74d2a99a5f
Alex Wilmer <djazium@gmail.com>
2016-07-11 21:08:09 -0400

    find piano sample by velocity

--- src/plugins/Piano/PianoSource.js
+++ src/plugins/Piano/PianoSource.js
@@ -15,9 +15,13 @@ export default class PianoSource {
     // TODO: load single soundfont file / chunks
     // https://www.clicktorelease.com/code/typed-array-web-audio/
 
-    let samples = _.range(9, 97).map(x => ({
-      id: `${x}-80`, audio: `${API_URL}/piano/${x}-80.mp3`
-    }))
+    let samples = _.flatMap(
+      _.range(4, 79).map(x =>
+        [30, 50, 80, 120].map(v => ({
+          id: `${x}-${v}`, audio: `${API_URL}/piano/${x}-${v}.mp3`,
+        }))
+      )
+    )
 
     samples.forEach(sample =>
       load(this.ctx, sample).then(result => {
@@ -35,22 +39,30 @@ export default class PianoSource {
   }
 
   fireNote(keyNum, velocity, time = 0, detune) {
-    // TODO: get correct note by velocity range
-    let buffer = this.bufferMap[`${keyNum}-80`]
+    let nearestSample = velocity < 31
+      ? 30 : velocity < 51
+      ? 50 : velocity < 81
+      ? 80 : 120
+
+    let buffer = this.bufferMap[`${keyNum - 8}-${nearestSample}`]
 
     if (!buffer) return
 
-    let active = this.activeSources.find(x => x.keyNum === keyNum)
-    if (!velocity && active) {
-      // Schedule Release
-      let now = this.ctx.currentTime
-      let amplitude = active.sourceGain.gain
-      amplitude.cancelScheduledValues(now)
-      amplitude.setValueAtTime(amplitude.value, now)
-      amplitude.linearRampToValueAtTime(0, now + 0.35)
+    if (!velocity) { // note off
+      let active = this.activeSources.find(x => x.keyNum === keyNum)
 
-      // Dispose of source
-      this.activeSources = this.activeSources.filter(x => x.keyNum !== keyNum)
+      if (active) {
+
+        // Schedule Release
+        let now = this.ctx.currentTime
+        let amplitude = active.sourceGain.gain
+        amplitude.cancelScheduledValues(now)
+        amplitude.setValueAtTime(amplitude.value, now)
+        amplitude.linearRampToValueAtTime(0, now + 0.35)
+
+        // Dispose of source
+        this.activeSources = this.activeSources.filter(x => x.keyNum !== keyNum)
+      }
     }
     else if (velocity && !active) {
       // Create source + volume for ADSR
