c48804fcf97ec1d8945a7a389c15b8d948f1c749
Jordan Nieuwhof <jnieuwhof@assetmine.ky>
2016-07-25 11:41:37 -0400

    Testing idea of how to accomplish max meter level display

--- src/components/MixerTrackLevel.js
+++ src/components/MixerTrackLevel.js
@@ -4,19 +4,29 @@ import engineShape   from '../audio/AudioEnginePropTypes.js'
 
 export class MixerTrackLevel extends Component {
 
+  percentFromDb(dB) {
+    return dB < -60 ? 0 : 100 * (dB + 60) / 60
+  }
+
   render() {
     let muteTrack = this.props.atleastOneTrackSoloed && !this.props.track.solo || this.props.track.mute
     let trackLevelClass = "mixer-track-level"
         trackLevelClass += muteTrack ? " mixer-track-level-muted" : ''
-    let dB = this.props.ENGINE.getTrackOutputDecibels(this.props.track.id)
-    let left = dB < -60 ? 0 : 100*(dB + 60)/60
-    let limitStyle = {
-      left: left+'%'
+    let left = this.percentFromDb(this.state.dB)
+    let right = this.percentFromDb(this.state.maxDb.level)
+    let width = right > left ? right - left - 2 : 0
+    let leftLimitStyle = {
+      left: `${left}%`,
+      width: `${width}%`
+    }
+    let rightLimitStyle = {
+      left: `${right}%`
     }
 
     return (
       <div className={trackLevelClass}>
-        <div className="mixer-track-meter" style={limitStyle} />
+        <div className="mixer-track-meter" style={leftLimitStyle} />
+        <div className="mixer-track-meter" style={rightLimitStyle} />
       </div>
     )
   }
@@ -28,6 +38,10 @@ export class MixerTrackLevel extends Component {
 
   shouldComponentUpdate() {
     return false
+  }
+
+  componentWillMount() {
+    this.state = { maxDb: { level: -Infinity, expiry: null }}
   }
 
   componentDidMount() {
@@ -42,7 +56,29 @@ export class MixerTrackLevel extends Component {
     if (this.unmounted)
       return
 
-    this.forceUpdate()
+    let shouldUpdate = false
+    let dB = Math.round(this.props.ENGINE.getTrackOutputDecibels(this.props.track.id) * 100) / 100
+    let now = Date.now()
+    if (dB > -Infinity && dB >= this.state.maxDb.level) {
+      this.setState({ maxDb: { level: dB, expiry: now + 3000 }})
+      shouldUpdate = true
+    }
+    else if (this.state.maxDb.expiry && this.state.maxDb.expiry < now) {
+      if(dB > -Infinity)
+        this.setState({ maxDb: { level: dB, expiry: now + 3000 }})
+      else
+        this.setState({ maxDb: { level: -Infinity, expiry: null}})
+      shouldUpdate = true
+    }
+
+    if (dB !== this.state.dB) {
+      shouldUpdate = true
+      this.setState({ dB })
+    }
+
+
+    if (shouldUpdate)
+      this.forceUpdate()
     requestAnimationFrame(this.renderMeter)
   }
 
