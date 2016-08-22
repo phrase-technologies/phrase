c30f9895320d226430b1c11192c42e26253158ff
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-08-22 11:51:08 -0400

    Rebased develop into ajaxHelpers

--- src/helpers/ajaxHelpers.js
+++ src/helpers/ajaxHelpers.js
@@ -2,24 +2,31 @@
 
 import 'whatwg-fetch' // `fetch` polyfill for Safari
 
-export let api = async ({ endpoint, body }) => {
+export let phraseFetch = async({ endpoint, body }) => {
   let response
   try {
-    response = await fetch(`${API_URL}/api/${endpoint}`, {
+    response = await fetch(`${API_URL}/${endpoint}`, {
       method: `POST`,
       headers: { 'Content-Type': `application/json` },
-      body: JSON.stringify({
-        token: localStorage.token,
-        email: localStorage.email,
-        username: localStorage.username,
-        userId: localStorage.userId,
-        ...(body || {})
-      }),
+      body: JSON.stringify(body),
     })
   } catch (e) {
     throw { status: 504, statusText: "Connection failure" } // Request failed / Timeout
   }
-
   if (response.ok) return response.json()
   throw response
+}
+
+export let api = async ({ endpoint, body }) => {
+  let response = await phraseFetch({
+    endpoint: `api/${endpoint}`,
+    body: {
+      token: localStorage.token,
+      email: localStorage.email,
+      username: localStorage.username,
+      userId: localStorage.userId,
+      ...(body || {})
+    },
+  })
+  return response
 }
