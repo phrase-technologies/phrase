d1ab9f4e87c3a3b2c73aa76b4ff36dafd460e820
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-08-24 13:28:15 -0400

    Removed some unnecessary math

--- src/reducers/reduceTransport.js
+++ src/reducers/reduceTransport.js
@@ -8,12 +8,12 @@ import { mixerScrollX } from 'reducers/reduceMixer'
 const getNewScroll = ({ min, max, playhead, barCount }) => {
   let viewWidth = max - min
   let playheadPercent = playhead / barCount
-  if (max < 1 && ((max - playheadPercent) / viewWidth) < 0) {
+  if (max < 1 && (max - playheadPercent) < 0) {
     let newMax = Math.min(1, playheadPercent + viewWidth)
     let offset = (newMax < 1) ? 0.05 * viewWidth : 0
     return { min: newMax - viewWidth - offset, max: newMax - offset }
   }
-  else if (min > 0 && ((playheadPercent - min) / viewWidth) < 0) {
+  else if (min > 0 && (playheadPercent - min) < 0) {
     let newMin = Math.max(0, playheadPercent - viewWidth)
     let offset = (newMin > 0) ? 0.05 * viewWidth : 0
     return { min: newMin + offset, max: newMin + viewWidth + offset }
