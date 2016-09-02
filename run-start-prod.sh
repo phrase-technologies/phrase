e67f263ae900d96ed3852c30cc320a3c90a92eed
Anson Kao <anson.kao@gmail.com>
2016-09-02 03:52:30 -0400

    OPS-10 #time 1h Testing deploy and needs to default to port 80 for safety

--- run-start-prod.sh
+++ run-start-prod.sh
@@ -1,6 +1,34 @@
-#!/usr/bin/env bash
+# =============================================================================
+# Production Build/Serve Script
+# =============================================================================
+# This script is used to serve the API. There is no
+# separate build script, just this one.
+
+# =============================================================================
+# Color Codes
+# =============================================================================
+RED="\033[31m"
+YELLOW="\033[33m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
+# =============================================================================
+# Script
+# =============================================================================
+# PORT is not required, defaults to 80
+if [ -z ${PORT+x} ]; then
+  FINAL_PORT=80
+else
+  FINAL_PORT=$PORT
+fi
+
+# CLIENT_URL is required
 if [ -z ${CLIENT_URL+x} ]; then
+  printf "$RED"
   echo "CLIENT_URL must be defined. Try this format: CLIENT_URL=phrase.fm npm run start-prod"
 else
-  NODE_ENV=production node server.forever.js
+  printf "$YELLOW"
+  echo "⌛ Launching with CLIENT_URL = $CLIENT_URL and PORT = $FINAL_PORT"
+  printf "$RESET"
+  NODE_ENV=production PORT=$FINAL_PORT node server.forever.js
 fi
