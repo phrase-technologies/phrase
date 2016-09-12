5a3b1e8bed04fadc33ca2922c483cc12ad188ac6
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-12 10:02:32 -0400

    Started database refactor, more to come

---
+++ src/helpers/db-helpers/r-user-update-from-email.js
@@ -0,0 +1,11 @@
+import r from 'rethinkdb'
+
+export default async (db, { email, update }) => {
+  if (!email || !update) return null
+  let result = await r.table(`users`)
+    .getAll(email, { index: `email` })
+    .limit(1)
+    .update(update)
+    .run(db)
+  return result
+}
