96220167f623a1d24a8cf16c4e2d0545374a9624
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-13 17:51:25 -0400

    oauth-callback can now differentiate a facebook auth error from a user access issue

--- src/helpers/oAuth.js
+++ src/helpers/oAuth.js
@@ -3,7 +3,7 @@ import { clientURL } from '../config'
 export let getOAuthCallbackURL = (user) => {
   let redirectUrl = `http://${clientURL}/oauth-callback?`
   if (!user)
-    redirectUrl += `success=false`
+    redirectUrl += `error=true`
   else {
     Object.keys(user).forEach(key => {
       redirectUrl += `${key}=${user[key]}&`
