f021d3ddf24df33d67706b4bcf4b7e64aa93da08
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-15 16:27:57 -0400

    Refactored facebook oAuth code to be reused by other strategies

--- src/auth/strategies/facebook.js
+++ src/auth/strategies/facebook.js
@@ -2,69 +2,20 @@
 import passport from 'passport'
 import FacebookStrategy from 'passport-facebook'
 import { facebookAppID, facebookAppSecret, apiURL } from '../../config'
-import {
-  rUserGetFromEmail,
-  rOAuthInsert,
-  rUserUpdate,
-  rOAuthDeleteFromEmail,
-} from '../../helpers/db-helpers'
-import { generateUniqueToken } from '../../helpers/token'
-import { completeOAuth } from '../../helpers/oAuth'
+import { completeOAuth, handleOAuth } from '../../helpers/oAuth'
 
 export default ({ app, db, io }) => {
-  passport.use(new FacebookStrategy({
-    clientID: facebookAppID,
-    clientSecret: facebookAppSecret,
-    callbackURL: `${apiURL}/auth/facebook/callback`,
-    profileFields: [`id`, `emails`, `name`, `picture`],
-  }, async (accessToken, refreshToken, profile, done) => {
-      try {
-        let { emails, photos } = profile
-        if (!photos || !emails || !accessToken) {
-          done(null, { success: false })
-          return
-        }
-
-        let { value: email } = emails[0]
-        let { value: picture } = photos[0]
-        let lowerCaseEmail = email.toLowerCase()
-        let facebook = {
-          accessToken,
-          refreshToken: !refreshToken ? null : refreshToken,
-        }
-        let oAuthToken = await generateUniqueToken({ table: `oAuth`, index: `oAuthToken`, db })
-        let user = await rUserGetFromEmail(db, { email })
-
-
-        await rOAuthDeleteFromEmail(db, { email })
-        await rOAuthInsert(db, {
-          oAuthToken,
-          email,
-          facebook,
-          picture,
-        })
-
-        if (user) // If user exists update their info
-          await rUserUpdate(db, {
-            id: user.id,
-            update: {
-              facebook,
-              picture,
-            },
-          })
-
-        done(null, {
-          success: true,
-          email: lowerCaseEmail,
-          token: oAuthToken,
-          newUser: !user || !user.username,
-        })
-      }
-      catch (e) {
-        console.log(e)
-        done(true)
-      }
-  }))
+  passport.use(
+    new FacebookStrategy({
+      clientID: facebookAppID,
+      clientSecret: facebookAppSecret,
+      callbackURL: `${apiURL}/auth/facebook/callback`,
+      profileFields: [`id`, `emails`, `name`, `picture`],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      handleOAuth({ profile, done, db })
+    }
+  ))
 
   app.get(`/auth/facebook`,
     passport.authenticate(`facebook`, {
@@ -77,7 +28,7 @@ export default ({ app, db, io }) => {
   app.get(`/auth/facebook/callback`, (req, res, next) => {
     passport.authenticate(`facebook`,
       { session: false },
-      (err, user) => { completeOAuth(res, io, err, user) },
+      (err, user) => { completeOAuth({ res, io, err, user }) },
     )(req, res, next)
   })
 }
--- src/helpers/oAuth.js
+++ src/helpers/oAuth.js
@@ -1,5 +1,13 @@
+import {
+  rUserGetFromEmail,
+  rOAuthInsert,
+  rUserUpdate,
+  rOAuthDeleteFromEmail,
+} from './db-helpers'
 
-export let completeOAuth = (res, io, err, user) => {
+import { generateUniqueToken } from './token'
+
+export let completeOAuth = ({ res, io, err, user }) => {
   if (!user) {
     console.log(err)
     io.emit(`server::oAuthUser`, { error: true })
@@ -8,4 +16,45 @@ export let completeOAuth = (res, io, err, user) => {
 
   res.send(`<script type="text/javascript">window.close()</script>`)
   res.end()
+}
+
+export let handleOAuth = async ({ profile, done, db }) => {
+  try {
+    let { emails, photos } = profile
+    if (!photos || !emails) {
+      done(null, { success: false })
+      return
+    }
+
+    let { value: email } = emails[0]
+    let { value: picture } = photos[0]
+
+    let lowerCaseEmail = email.toLowerCase()
+    let oAuthToken = await generateUniqueToken({ table: `oAuth`, index: `oAuthToken`, db })
+    let user = await rUserGetFromEmail(db, { email })
+
+    await rOAuthDeleteFromEmail(db, { email })
+    await rOAuthInsert(db, {
+      email,
+      oAuthToken,
+      picture,
+    })
+
+    if (user) // If user exists update their info
+      await rUserUpdate(db, {
+        id: user.id,
+        update: { picture },
+      })
+
+    done(null, {
+      success: true,
+      email: lowerCaseEmail,
+      token: oAuthToken,
+      newUser: !user || !user.username,
+    })
+  }
+  catch (e) {
+    console.log(e)
+    done(true)
+  }
 }
