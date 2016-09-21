1e095b530f3ce65d1b8a1b12a71495e28aa72e13
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-21 01:45:40 -0400

    Moved uploadProfilePic endpoint to authorized, uploadProfilePic now works with base64 image upload

--- src/routes/authorized/index.js
+++ src/routes/authorized/index.js
@@ -1,9 +1,11 @@
 import save from './save'
 import update from './update'
 import deletePhrase from './deletePhrase'
+import uploadProfilePic from './uploadProfilePic'
 
 export default [
   save,
   update,
   deletePhrase,
+  uploadProfilePic,
 ]
