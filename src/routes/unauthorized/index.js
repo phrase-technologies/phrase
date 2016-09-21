efcbd0d2baf0d083b61bce09d6e538fa22b69aac
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-21 12:38:12 -0400

    Merged authhorized endpoints, continuing rebase

--- src/routes/unauthorized/index.js
+++ src/routes/unauthorized/index.js
@@ -2,12 +2,10 @@ import load from './load'
 import loadOne from './loadOne'
 import loadUserPhrases from './loadUserPhrases'
 import signupMicList from './signup-mic-list'
-import uploadProfilePic from './uploadProfilePic'
 
 export default [
   load,
   loadOne,
   loadUserPhrases,
   signupMicList,
-  uploadProfilePic,
 ]
