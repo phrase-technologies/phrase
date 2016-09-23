177e80ead2de085cc69bb2caf6198cd273c24892
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-23 02:23:36 -0400

    Standardized migration scripts, added checks to automatically detect migrations, added script to automatically run migrations

--- src/setup/index.js
+++ src/setup/index.js
@@ -1,7 +1,9 @@
 import setupDatabase from './setupDatabase'
 import setupSocketConnection from './setupSocketConnection'
+import getNewMigrations from './getNewMigrations'
 
 export {
   setupDatabase,
   setupSocketConnection,
+  getNewMigrations,
 }
