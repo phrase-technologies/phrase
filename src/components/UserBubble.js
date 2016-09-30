45779bf12a6a55019cca9ea25a8df7c7084350a7
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-09-30 00:55:13 -0400

    Removed unnecessary userId prop check

--- src/components/UserBubble.js
+++ src/components/UserBubble.js
@@ -5,21 +5,18 @@ import { userRequestProfileIfNotExisting } from 'reducers/reduceUserProfile'
 
 export class UserBubble extends Component {
   componentWillMount() {
-    if (this.props.userId)
-      this.props.dispatch(userRequestProfileIfNotExisting({ userId: this.props.userId }))
+    this.props.dispatch(userRequestProfileIfNotExisting({ userId: this.props.userId }))
   }
 
   render() {
+    let user = this.props.users[this.props.userId]
     let bubble
-    if (this.props.userId) {
-      let user = this.props.users[this.props.userId]
-      if (!user || user.pending)
-        bubble = <span className="fa fa-spinner fa-pulse" />
-      else if (user.picture)
-        bubble = <img src={user.picture} />
-      else
-        bubble = user.username.substr(0,2).toUpperCase()
-    }
+    if (!user || user.pending)
+      bubble = <span className="fa fa-spinner fa-pulse" />
+    else if (user.picture)
+      bubble = <img src={user.picture} />
+    else
+      bubble = user.username.substr(0,2).toUpperCase()
 
     return (
       <div
