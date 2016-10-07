167fe4b22f604390d35e900540bd9b6518195134
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-10-07 11:08:39 -0400

    Improved upload error handling, added more robust audio file type validation

--- src/routes/authorized/uploadTrackAudio.js
+++ src/routes/authorized/uploadTrackAudio.js
@@ -1,28 +1,44 @@
 import fs from 'fs'
 import path from 'path'
+import readChunk from 'read-chunk'
+import fileType from 'file-type'
 
 export default ({ api }) => {
   api.post(`/uploadTrackAudio`, async (req, res) => {
     let file
     if (!req.files || !(file = req.files[0])) {
-      return res.status(500).json({ error: `Missing audio files` })
+      return res.json({
+        error: `Missing audio file`,
+        message: `Please try again`,
+      })
     }
     let extensions = [ `mp3` ]
-    let extension = file.mimetype.replace(`audio/`, ``)
+    let extension
+    try {
+      let buffer = readChunk.sync(file.path, 0, 262)
+      extension = fileType(buffer).ext // Validate audio file type by magic numbers
+    }
+    catch (e) {
+      fs.unlink(file.path)
+      return res.status(500)
+    }
+
     if (!extensions.includes(extension)) {
       fs.unlink(file.path)
-      return res.status(500).json({ error: `Invalid audio file` })
+      return res.json({
+        title: `Invalid audio file`,
+        message: `Valid file types: ${extensions.join(`,`)}`,
+      })
     }
-    let { phraseId, authorId, clipId } = req.body
-    let fileName = `${phraseId}_${authorId}_${clipId}.${extension}`
+    let { phraseId } = req.body
+    let fileName = `${phraseId}_${Date.now()}.${extension}`
     let filePath = path.join(`public/audio-tracks`, fileName)
-
     try {
       fs.renameSync(file.path, filePath)
-    } catch(error) {
+    }
+    catch(error) {
       if (fs.existsSync(file.path))
         fs.unlink(file.path)
-      console.log(error)
       return res.status(500).json({ error })
     }
 
