d2edcf7a4de0c9f1ea0af5727987dff2bd8d038b
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-10-12 13:43:57 -0400

    Added initial implementation of m4a conversion / support

--- src/routes/authorized/uploadTrackAudio.js
+++ src/routes/authorized/uploadTrackAudio.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import readChunk from 'read-chunk'
 import fileType from 'file-type'
+import ffmpeg from 'fluent-ffmpeg'
 
 import { rPhraseGet } from '../../helpers/db-helpers'
 
@@ -37,7 +38,7 @@ export default ({ api, db }) => {
         return res.status(403).json({ message: `You do not have permission to edit this phrase` })
       }
 
-      let extensions = [ `mp3`, `wav`, `ogg` ]
+      let extensions = [ `mp3`, `wav`, `ogg`, `m4a` ]
       let extension
       try {
         let buffer = readChunk.sync(file.path, 0, 262)
@@ -55,10 +56,37 @@ export default ({ api, db }) => {
           message: `Valid file types: ${extensions.join(`,`)}`,
         })
       }
+
+      let inputPath = file.path
+
+      if (extension === `m4a`) {
+        let output = `${file.path}.mp3`
+        let source = path.resolve(file.path)
+        await Promise.all([
+          new Promise((resolve, reject) => {
+            ffmpeg({ source })
+              .output(output)
+              .audioChannels(2)
+              .format(`mp3`)
+              .on(`error`, (error) => {
+                reject(error)
+              })
+              .on(`end`, () => {
+                fs.unlinkSync(inputPath)
+                inputPath = output
+                extension = `mp3`
+                resolve()
+              })
+              .run()
+          }),
+        ])
+      }
+
+
       let fileName = `${phraseId}_${Date.now()}.${extension}`
       let filePath = path.join(`public/audio-tracks`, fileName)
       try {
-        fs.renameSync(file.path, filePath)
+        fs.renameSync(inputPath, filePath)
       }
       catch(error) {
         if (fs.existsSync(file.path))
