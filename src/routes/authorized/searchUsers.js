17f015334d00ea9886d37944ddf333b14b61be31
Anson Kao <anson.kao@gmail.com>
2016-10-20 18:46:34 -0400

    WEB-156 #time 1h Autocomplete users by both email and username effectively

--- src/routes/authorized/searchUsers.js
+++ src/routes/authorized/searchUsers.js
@@ -3,13 +3,14 @@ import r from 'rethinkdb'
 export default ({ api, db }) => {
   api.post(`/searchUsers`, async (req, res) => {
     let { searchTerm } = req.body
+    let searchRegex = `(?i)^${searchTerm}`
 
     if (!searchTerm) {
       return res.json({ success: false, message: `Search term cannot be empty.`})
     }
 
     let userCursor = await r.table(`users`).filter(row =>
-      row(`username`).match(searchTerm).or(row(`email`).match(searchTerm))
+      row(`username`).match(searchRegex).or(row(`email`).match(searchRegex))
     ).run(db)
 
     let users = await userCursor.toArray()
