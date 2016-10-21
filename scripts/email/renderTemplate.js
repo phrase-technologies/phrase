3f11ce4c7aabc1d857b56801a5848e28d71514fd
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2016-10-21 19:56:52 -0400

    Created a tool to render email templates to local html files, useful for template building

---
+++ scripts/email/renderTemplate.js
@@ -0,0 +1,46 @@
+
+import fs from 'fs'
+import path from 'path'
+import opener from 'opener'
+
+import renderEmail from '../../src/helpers/renderEmail'
+
+let baseDir = `scripts/email`
+
+let main = async (args) => {
+  try {
+    let template = args[0]
+    if (!template) {
+      console.log(`Please provide a template name`)
+      return
+    }
+
+    let templateDir = path.resolve(`templates`, `${template}`)
+    if (!fs.existsSync(templateDir)) {
+      console.log(`Template directory not found: ${templateDir}`)
+      return
+    }
+    console.log(`Using template directory: ${templateDir}`)
+
+    let dataFile = path.resolve(baseDir, `test-data`, `${template}.json`)
+    if(!fs.existsSync(dataFile)) {
+      console.log(`Please create the following data file: ${dataFile}`)
+      return
+    }
+    console.log(`Using test file: ${dataFile}`)
+
+    let outputFile = path.resolve(baseDir, `output`, `${template}_${Date.now()}.html`)
+    console.log(`Using output file: ${outputFile}`)
+
+    let data = JSON.parse(fs.readFileSync(dataFile, 'utf8'))
+    let html = await renderEmail({ template, data })
+    fs.writeFileSync(outputFile, html)
+
+    opener(outputFile)
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+main(process.argv.slice(2))
