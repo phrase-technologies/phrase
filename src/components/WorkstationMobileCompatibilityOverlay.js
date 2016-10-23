d96b534900fbefd7cc7a95bd8e4d7e974a998a23
Dien Nguyen <DN@Diens-MacBook-Pro-2.local>
2016-10-23 13:48:23 -0400

    Converting tabs to spaces -haha Anson

--- src/components/WorkstationMobileCompatibilityOverlay.js
+++ src/components/WorkstationMobileCompatibilityOverlay.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react'
 import Modal from 'react-bootstrap/lib/Modal'
 
 export default class WorkstationMobileCompatibilityOverlay extends Component {
-	render() {
-		let modalProps = {
-			show: true,
-			container: this
-		}
+  render() {
+    let modalProps = {
+      show: true,
+      container: this
+    }
 
-		return (
-			<div id="workstation-mobile-compatibility-overlay">
-				<Modal {...modalProps}>
-					<Modal.Header>
-						<Modal.Title>Notice!</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						<p>Mobile/Tablet Compatibility coming soon.</p>
-						<p>Please visit phrase.fm from a desktop browser for the full experience!</p>
-					</Modal.Body>
-				</Modal>
-			</div>
-		)
-	}
+    return (
+      <div id="workstation-mobile-compatibility-overlay">
+        <Modal {...modalProps}>
+          <Modal.Header>
+            <Modal.Title>Notice!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Mobile/Tablet Compatibility coming soon.</p>
+            <p>Please visit phrase.fm from a desktop browser for the full experience!</p>
+          </Modal.Body>
+        </Modal>
+      </div>
+    )
+  }
 }
